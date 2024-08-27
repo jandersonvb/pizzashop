0e488894b7049f384f44726f7a6635f6f808e75a
jandersonvb <jandersonvb.dev@gmail.com>
2024-08-27 16:12:15 -0300

    feat: create functionality SignOut

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,4 +1,15 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { DialogDescription } from '@radix-ui/react-dialog'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import {
+  getManagedRestaurant,
+  GetManagedRestaurantResponse,
+} from '@/api/get-managed-restaurant'
+import { updateProfile } from '@/api/update-profile'
 
 import { Button } from './ui/button'
 import {
@@ -11,32 +22,22 @@ import {
 import { Input } from './ui/input'
 import { Label } from './ui/label'
 import { Textarea } from './ui/textarea'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { getManagedRestaurant, GetManagedRestaurantResponse } from '@/api/get-managed-restaurant'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { updateProfile } from '@/api/update-profile'
-import { toast } from 'sonner'
 
 const storeProfileSchema = z.object({
   name: z.string().min(3),
-  description: z.string().nullable()
+  description: z.string().nullable(),
 })
 
 type StoreProfileSchema = z.infer<typeof storeProfileSchema>
 
-
 export function StoreProfileDialog() {
   const queryClient = useQueryClient()
 
-
-  const { data: managedRestaurant } =
-    useQuery({
-      queryKey: ['managed-restaurant'],
-      queryFn: getManagedRestaurant,
-      staleTime: Infinity,
-    })
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+    staleTime: Infinity,
+  })
 
   const {
     register,
@@ -47,18 +48,26 @@ export function StoreProfileDialog() {
     values: {
       name: managedRestaurant?.name ?? '',
       description: managedRestaurant?.description ?? '',
-    }
+    },
   })
 
-  function updateManagedRestaurantCache({ name, description }: StoreProfileSchema) {
-    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>(['managed-restaurant'])
+  function updateManagedRestaurantCache({
+    name,
+    description,
+  }: StoreProfileSchema) {
+    const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
+      'managed-restaurant',
+    ])
 
     if (cached) {
-      queryClient.setQueryData<GetManagedRestaurantResponse>(['managed-restaurant'], {
-        ...cached,
-        name,
-        description,
-      })
+      queryClient.setQueryData<GetManagedRestaurantResponse>(
+        ['managed-restaurant'],
+        {
+          ...cached,
+          name,
+          description,
+        },
+      )
     }
 
     return { cached } // Return the previous value to be used in onMutate
@@ -75,8 +84,7 @@ export function StoreProfileDialog() {
       if (context?.previousProfile) {
         updateManagedRestaurantCache(context.previousProfile)
       }
-
-    }
+    },
   })
 
   async function handleUpdateProfile(data: StoreProfileSchema) {
@@ -91,7 +99,6 @@ export function StoreProfileDialog() {
     }
   }
 
-
   return (
     <DialogContent>
       <DialogHeader>
@@ -101,21 +108,28 @@ export function StoreProfileDialog() {
         </DialogDescription>
       </DialogHeader>
 
-      <form
-        onSubmit={handleSubmit(handleUpdateProfile)}
-      >
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
         <div className="space-y-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="name">
               Nome
             </Label>
-            <Input className="col-span-3" id="name" type="text" {...register('name')} />
+            <Input
+              className="col-span-3"
+              id="name"
+              type="text"
+              {...register('name')}
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="description">
               Descrição
             </Label>
-            <Textarea className="col-span-3" id="description" {...register('description')} />
+            <Textarea
+              className="col-span-3"
+              id="description"
+              {...register('description')}
+            />
           </div>
         </div>
 

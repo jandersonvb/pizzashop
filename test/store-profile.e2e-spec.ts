07ba70f2dd3212f158e01ae1dd4e257e16c454fb
jandersonvb <jandersonvb.dev@gmail.com>
2024-09-24 14:35:40 -0300

    test: create the E2E test from the Update profile

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test'
+
+test('update profile successfuly', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill('Rocket Pizza')
+  await page.getByLabel('Descrição').fill('A melhor pizza do mundo')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso')
+
+  await expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+
+  await expect(page.getByRole('button', { name: 'Rocket Pizza' })).toBeVisible()
+})
